c66544fd6b11461b8b2929f69d96a2cf37d33550
Connor <connor.cs@protonmail.com>
2023-08-17 20:14:56 -0400

    failure

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,48 @@
 import { SpotifyApi } from '@spotify/web-api-ts-sdk';
 import './App.css'
 import Login from './Login';
-
+import { useEffect, useState } from 'react';
 //take user input, artist, song, album, set it to state
 //
 
 function App() {
 
-  const api = SpotifyApi.withClientCredentials(
-    '1553a231a3b74e48bb3dc6efdce3cb72', '37da88f137294d5a9f6a7ea57f0d4be9'
-  )
+  const clientID = '1553a231a3b74e48bb3dc6efdce3cb72'
+  const clientSecret = '37da88f137294d5a9f6a7ea57f0d4be9'
 
-  // async function getArtist() {
-  //   const data = await fetch('https://api.spotify.com/v1/artists')
+  // const api = SpotifyApi.withClientCredentials(
+  //   '1553a231a3b74e48bb3dc6efdce3cb72', '37da88f137294d5a9f6a7ea57f0d4be9'
+  // )
+
+  const [token, setToken] = useState('')
+
+  // useEffect(() => {
+  //   const authParams = {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/x-www-form/urlencoded'
+  //     },
+  //     body: 'grant_type=client_credentials&client_id=' + clientID + '&client_secret=' + clientSecret
+  //   }
+  //   fetch('https://accounts.spotify.com/api/token', authParams)
+  //   .then(res=>res.json())
+  //   .then(data=>console.log(data))
+  // }, [])
+
+  // async function submit() {
+  //   const items = await api.search('Greta Van Fleet', ['artist']);
+  //   console.table(
+  //     items.artists.items.map((item) => ({
+  //       name: item.name,
+  //       followers: item.followers.total,
+  //       popularity: item.popularity,
+  //     }))
+  //   );
   // }
-
-  async function submit() {
-    const items = await api.search('Greta Van Fleet', ['artist']);
-    console.table(
-      items.artists.items.map((item) => ({
-        name: item.name,
-        followers: item.followers.total,
-        popularity: item.popularity,
-      }))
-    );
-  }
 
   return (
     <>
-      <button onClick={()=>submit()}>Submit</button>
+      {/* <button onClick={()=>submit()}>Submit</button> */}
       <Login />
     </>
   )
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=1553a231a3b74e48bb3dc6efdce3cb72&response_type=code&redirect_uri=http://localhost:5173/callback&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+const clientID = '1553a231a3b74e48bb3dc6efdce3cb72'
+const clientSecret = '37da88f137294d5a9f6a7ea57f0d4be9'
+const redirectURI = 'http://localhost:5173/callback'
+
+
+//the docs say to set response_type to code, instead of token, but code doesn't give me expected response
+//the response is supposed to include an authcode (that can be exchanged for access token) and a value called state inside the url, but does not include state value when i set respose type to code
+const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=' + clientID + '&response_type=token&redirect_uri=' + redirectURI + '&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state'
+
+async function testFunction () {
+    fetch(AUTH_URL)
+    .then(res=>res.json())
+    .then(data=>console.log(data))
+}
 
 export default function Login() {
     return (
         <>
             <div>Login</div>
-            <a href={AUTH_URL}>click to login</a>
+            {/* <a href={AUTH_URL}>click to login</a> */}
+            <button onClick={()=>testFunction()}>log in</button>
         </>
     )
 }
